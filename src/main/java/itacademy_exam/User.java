f6890173d68de3ae037e1f25aef9fed33140662c
Federico Cantore <f.cantore85@gmail.com>
2026-01-26 11:08:30 +0100

    add equals and hashcode to User

--- src/main/java/itacademy_exam/User.java
+++ src/main/java/itacademy_exam/User.java
@@ -1,5 +1,7 @@
 package itacademy_exam;
 
+import java.util.Objects;
+
 public class User
 {
     private String name;
@@ -16,6 +18,18 @@ public class User
 
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, password);
     }
 
     @Override
